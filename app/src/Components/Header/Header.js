80270fb71a3d9a6710782a23bf0c7f8020a825cd
dharmeshbc2989 <dharmesh.thanki@kripya.com>
2024-04-02 16:16:31 +0530

    header update

--- app/src/Components/Header/Header.js
+++ app/src/Components/Header/Header.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
-import $ from 'jquery';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserProfileActionHandler } from '../../Redux/Actions/user/UserProfile';
-import { Dropdown, Space, Menu } from 'antd';
-import { DownOutlined, AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+
 function Header() {
     const Logo = '/images/logo.png';
     let dispatch = useDispatch();
@@ -16,110 +15,28 @@ function Header() {
             key: 'SubMenu',
             children: [
                 { key: '1', label: (<Link to={"/About-Us"}>About Us</Link>), },
-                { key: '2', label: (<Link to={"/Help"}>Help</Link>), },
-                { key: '3', label: (<Link to={"/Faq"}>Faq</Link>), },
-                { key: '4', label: (<Link to={"/Support"}>Support</Link>), },
-                { key: '5', label: (<Link to={"/Contact-us"}>Contact Us</Link>), },
+                { key: '2', label: (<Link to={"/Faq"}>Faq</Link>), },
+                { key: '3', label: (<Link to={"/Support"}>Support</Link>), },
+                { key: '4', label: (<Link to={"/Contact-us"}>Contact Us</Link>), },
             ],
         },
     ];
     const profile_menu = [
         {
-          label: 'Pages',
-          key: 'SubMenu',
-          icon: <SettingOutlined />,
-          children: [
-            {
-              type: 'group',
-              label: 'Item 1',
-              children: [
-                {
-                  label: 'Option 1',
-                  key: 'setting:1',
-                },
-                {
-                  label: 'Option 2',
-                  key: 'setting:2',
-                },
-              ],
-            },
-            {
-              type: 'group',
-              label: 'Item 2',
-              children: [
-                {
-                  label: 'Option 3',
-                  key: 'setting:3',
-                },
-                {
-                  label: 'Option 4',
-                  key: 'setting:4',
-                },
-              ],
-            },
-          ],
+            label: 'Pages',
+            key: 'SubMenu',
+            icon: <span className="user-icon ms-sm-2">
+                <img src={`http://127.0.0.1:8000${userProfileData?.avatar}`} style={{ height: '36px', width: '36px', borderRadius: '50%' }} />
+            </span>,
+            children: [
+                { key: '1', label: `Hi, ${userProfileData?.name}` },
+                { key: '2', label: (<Link to={"/profile"}>Personal Information</Link>) },
+                { key: '3', label: (<Link to={"/Help"}>Need Help?</Link>), },
+                { key: '4', label: (<Link onClick={() => logoutHandler()}>Log Out</Link>), },
+            ],
         },
-      ];
+    ];
 
-    useEffect(() => {
-        const handleDropdown = () => {
-            if (window.innerWidth > 991) {
-                $('.primary-menu ul.navbar-nav li.dropdown, .login-signup ul.navbar-nav li.dropdown').on("mouseover", function () {
-                    console.log("Dropdown show on hover")
-                    $(this).find('> .dropdown-menu').stop().slideDown('fast');
-                    $(this).bind('mouseleave', function () {
-                        $(this).find('> .dropdown-menu').stop().css('display', 'none');
-                    });
-
-                    // When dropdown going off to the out of the screen.
-                    $('.primary-menu ul.navbar-nav > li.dropdown > .dropdown-menu').each(function () {
-                        var menu = $('#header .header-row').offset();
-                        var dropdown = $(this).parent().offset();
-                        var i = (dropdown.left + $(this).outerWidth()) - (menu.left + $('#header .header-row').outerWidth());
-                        if (i > 0) {
-                            if ($("html").attr("dir") === 'rtl') {
-                                $(this).css('margin-right', '-' + (i) + 'px');
-                            } else {
-                                $(this).css('margin-left', '-' + (i) + 'px');
-                            }
-                        }
-                    });
-                });
-            }
-        };
-
-        const handleDropdownMenuPosition = () => {
-            $(".dropdown li").on('mouseenter mouseleave', function (e) {
-                console.log("mouseenter mouseleave");
-                if (window.innerWidth > 991) {
-                    if ($('.dropdown-menu', this).length) {
-                        var elm = $('.dropdown-menu', this);
-                        var off = elm.offset();
-                        var l = off.left;
-                        var w = elm.width();
-                        var docW = $(window).width();
-                        var lr = ($(window).width() - (off.left + elm.outerWidth())); //offset right
-                        var isEntirelyVisible = ($("html").attr("dir") === 'rtl') ? (lr + w + 30 <= docW) : (l + w + 30 <= docW);
-                        if (!isEntirelyVisible) {
-                            $(elm).addClass('dropdown-menu-end');
-                            $(elm).parents('.dropdown:first').find('> a.dropdown-toggle > .arrow').addClass('arrow-end');
-                        } else {
-                            $(elm).removeClass('dropdown-menu-end');
-                            $(elm).parents('.dropdown:first').find('> a.dropdown-toggle > .arrow').removeClass('arrow-end');
-                        }
-                    }
-                }
-            });
-        };
-
-        handleDropdown();
-        handleDropdownMenuPosition();
-
-        return () => {
-            $('.primary-menu ul.navbar-nav li.dropdown, .login-signup ul.navbar-nav li.dropdown').off("mouseover");
-            $(".dropdown li").off('mouseenter mouseleave');
-        };
-    }, []);
     const logoutHandler = () => {
         try {
             localStorage.clear();
@@ -152,22 +69,8 @@ function Header() {
                                 <ul className="navbar-nav">
                                     <li className="profile dropdown active">
                                         <a className="pe-0 dropdown-toggle" href="#" title="My Profile">
-                                            {/* <span className="d-none d-sm-inline-block">My Profile</span> */}
-                                            <Menu mode="horizontal" items={profile_menu} />
-                                            <span className="user-icon ms-sm-2">
-                                                <img src={`http://127.0.0.1:8000${userProfileData?.avatar}`} style={{ height: '36px', width: '36px', borderRadius: '50%' }} />
-                                            </span>
+                                            <Menu className="d-none d-sm-inline-block" mode="horizontal" items={profile_menu} />
                                         </a>
-                                        
-                                        {/* 
-                                        <ul className="dropdown-menu">
-                                            <li className="text-center text-3 py-2">Hi, {userProfileData?.name}</li>
-                                            <li className="dropdown-divider mx-n3"></li>
-                                            <li><Link className="dropdown-item" to={"profile"}><i className="fas fa-user"></i>Personal Information</Link></li>
-                                            <li className="dropdown-divider mx-n3"></li>
-                                            <li><a className="dropdown-item" href="help.html"><i className="fas fa-life-ring"></i>Need Help?</a></li>
-                                            <li><a className="dropdown-item" href="#" onClick={logoutHandler}><i className="fas fa-sign-out-alt"></i>Log Out</a></li>
-                                        </ul>*/}
                                     </li>
                                 </ul>
                             </nav>
